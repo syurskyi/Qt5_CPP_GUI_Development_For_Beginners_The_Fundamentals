beb9a44d295e6f6b4feec4be5f43bdac03b6b150
Grey <sergejyurskyj@yahoo.com>
2020-07-26 14:39:35 -0700

    Committed 1-11QtGui

---
+++ 2. C++ Beginner Crash Course/1-11QtGui/main.cpp
@@ -0,0 +1,15 @@
+//
+// Created by syurskyi on 7/26/2020.
+//
+
+#include "widget.h"
+#include <QApplication>
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    Widget w;
+    w.show();
+
+    return a.exec();
+}
