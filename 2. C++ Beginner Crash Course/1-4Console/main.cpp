8a7a40eed52cd9f36bbfb5eee7141dad4eba5e71
Grey <sergejyurskyj@yahoo.com>
2020-07-26 14:30:30 -0700

    Committed 1-4Console

---
+++ 2. C++ Beginner Crash Course/1-4Console/main.cpp
@@ -0,0 +1,14 @@
+//
+// Created by syurskyi on 7/26/2020.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello World!" << endl ;
+    cout << "How are you doing" << endl;
+    return 0;
+}
